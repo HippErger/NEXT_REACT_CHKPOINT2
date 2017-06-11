6dc8e40b5fdea270777f6f7d9dddf97525538852
HippErger <berger.clayton@gmail.com>
2017-06-10 18:57:32 -0600

    need to leave but added small logic to vehicles page

--- src/pages/vehiclespage.js
+++ src/pages/vehiclespage.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Link from 'next/link';
 
+/* When you get back Clayton, just remember that the Express Practice has these examples.
+
+We're going to create a stateless array and pass it to this page with a container
+
+and inside this page I will put in components like Vehicles that contains a collapsible mapper that contains a vehicle component for each item in the array. 
+*/
+
 function VehiclesPage() {
+  // const vehicle = props.resultData;
+  const vehicle = 0;
+  if (!vehicle) {
+    return (
+      <div>
+        <h4>Vehicle not found</h4>
+        <Link href="/">
+          <button>Home</button>
+        </Link>
+      </div>
+    );
+  }
   return (
     <div>
-      <h3>This is the vehicles page</h3>
+      <h4>{vehicle.make}</h4>
+      <ul>
+        <li>{vehicle.model}</li>
+        <li>{vehicle.year}</li>
+        <li>{vehicle.miles}</li>
+        <li>{vehicle.price}</li>
+      </ul>
       <Link href="/">
         <button>Home</button>
       </Link>
@@ -13,3 +38,12 @@ function VehiclesPage() {
 }
 
 export default VehiclesPage;
+
+// return (
+//   <div>
+//     <h3>This is the vehicles page</h3>
+//     <Link href="/">
+//       <button>Home</button>
+//     </Link>
+//   </div>
+// );
