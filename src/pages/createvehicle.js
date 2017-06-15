b04eb40b53f1eee445b20c5a073c2514fef23803
HippErger <berger.clayton@gmail.com>
2017-06-14 21:15:35 -0500

    Ivo taught me shorthand for bind and replaced them in createvehicle

--- src/pages/createvehicle.js
+++ src/pages/createvehicle.js
@@ -7,6 +7,18 @@ import {initStore} from '../store';
 class createVehicle extends Component {
   constructor() {
     super();
+    this.state = {
+      imgURL: '',
+      year: '',
+      make: '',
+      model: '',
+      price: '',
+      km: '',
+      miles: '',
+      fuel: '',
+      city: '',
+      newCar: ''
+    };
 
     // const INITIAL_STATE = {
     //   imgURL: '',
@@ -18,63 +30,128 @@ class createVehicle extends Component {
     //   miles: '',
     //   fuel: '',
     //   city: '',
-    //   newCar: Boolean
+    //   newCar: ''
     // };
   }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log('from create vehicle page', event);
+    this.props.onSubmit(
+      this.state
+    );
+  };
+
+  handleInputChange = event => {
+    console.log('hello', event.target.value);
+  }
+
   render() {
     return (
       <div>
         <Header />
         <h3>You can add a new vehicle here</h3>
-        <form onSubmit={event => {
-          event.preventDefault();
-          console.log('from create vehicle page', event);
-        }}>
-          <input type="text" name="imgUrl" placeholder="imgUrl" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            name="imgUrl"
+            placeholder="imgUrl"
+            onChange={this.handleInputChange}
+          />
           &nbsp; &nbsp;
-          <input type="text" name="year" placeholder="year"/>
+          <input
+            type="text"
+            name="year"
+            placeholder="year"
+            onChange={this.handleInputChange}
+          />
           <br />
           <br />
-          <input type="text" name="make" placeholder="make"/>
+          <input
+            type="text"
+            name="make"
+            placeholder="make"
+            onChange={this.handleInputChange}
+          />
           &nbsp; &nbsp;
-          <input type="text" name="model" placeholder="model"/>
+          <input
+            type="text"
+            name="model"
+            placeholder="model"
+            onChange={this.handleInputChange}
+          />
           <br />
           <br />
-          <input type="text" name="price" placeholder="price"/>
+          <input
+            type="text"
+            name="price"
+            placeholder="price"
+            onChange={this.handleInputChange}
+          />
           &nbsp; &nbsp;
-          <input type="text" name="km" placeholder="km"/>
+          <input
+            type="text"
+            name="km"
+            placeholder="km"
+            onChange={this.handleInputChange}
+          />
           <br />
           <br />
-          <input type="text" name="miles" placeholder="miles"/>
+          <input
+            type="text"
+            name="miles"
+            placeholder="miles"
+            onChange={this.handleInputChange}
+          />
           &nbsp; &nbsp;
-          <input type="text" name="fuel" placeholder="fuel"/>
+          <input
+            type="text"
+            name="fuel"
+            placeholder="fuel"
+            onChange={this.handleInputChange}
+          />
           <br />
           <br />
-          <input type="text" name="city" placeholder="city"/>
+          <input
+            type="text"
+            name="city"
+            placeholder="city"
+            onChange={this.handleInputChange}
+          />
           &nbsp; &nbsp;
-          <input type="text" name="newCar" placeholder="newCar"/>
+          <input
+            type="text"
+            name="newCar"
+            placeholder="True/False"
+            onChange={this.handleInputChange}
+          />
           <br />
           <br />
-          <input type="submit" value="Submit" />
+          <input
+            type="submit"
+            value="Submit"
+          />
              &nbsp; &nbsp;
         </form>
         <Link href="/vehiclespage">
-          <button >Take Me to All Cars</button>
+          <button >
+            Take Me to All Cars
+          </button>
         </Link>
         &nbsp; &nbsp;
         <Link href="/">
-          <button>Home</button>
+          <button>
+            Home
+          </button>
         </Link>
       </div>
     );
   }
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     onSubmit: () => {
-//
-//     }};
-// }
+function mapDispatchToProps() {
+  return {
+    onSubmit: values => {
+      console.log('Form was submitted', values);
+    }};
+}
 
-export default withRedux(initStore, null, null)(createVehicle);
+export default withRedux(initStore, null, mapDispatchToProps)(createVehicle);
