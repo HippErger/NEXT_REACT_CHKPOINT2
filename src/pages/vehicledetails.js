9dbedbf10ab120ef1a5381ed029038aa320798bd
HippErger <berger.clayton@gmail.com>
2017-06-19 14:20:19 -0500

    onMount func is working with componentDidMount on Vehicle Details

--- src/pages/vehicledetails.js
+++ src/pages/vehicledetails.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 import withRedux from 'next-redux-wrapper';
 import Header from '../components/Header';
 import {initStore} from '../store';
+import PropTypes from 'prop-types';
 
 class VehicleDetails extends Component {
   // put an onMount function that loads the details of this vehicle.
+  componentDidMount() {
+    this.props.onMount();
+  }
   render() {
     return (
       <div>
@@ -15,4 +19,17 @@ class VehicleDetails extends Component {
   }
 }
 
-export default withRedux(initStore, null, null)(VehicleDetails);
+VehicleDetails.propTypes = {
+  onMount: PropTypes.func.isRequired
+};
+
+/* eslint-disable no-unused-vars */
+function mapDispatchToProps(dispatch) {
+  return {
+    onMount: () => {
+      console.log('vehicle detail page did load');
+    }
+  };
+}
+
+export default withRedux(initStore, null, mapDispatchToProps)(VehicleDetails);
